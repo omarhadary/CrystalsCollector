ee3ba8982631c67970aeaf1097fab47d72957da6
Our Laptop <Our Laptop>
2016-10-18 02:04:22 -0700

    crystals game logic mostly complete with reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,91 @@
+$(document).ready(function() {
+    startGame();
+    var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    var randomValue = [];
+    var yourScore = 0;
+    var losses = 0;
+    var wins = 0;
+    var greenCrystal;
+    var purpleCrystal;
+    var yellowCrystal;
+    var redCrystal;
+    // show random numbers at start of game
+    function reset() {
+        $('#crystals').text("");
+        yourScore = 0;
+        $('#your-score').text(0);
+        startGame();
+    }
 
-$(document).ready(function(){
-
-    
-
-    // var luke = '<div class="iviewer_image_mask" ' +  
-    //       'src: url(http://vignette3.wikia.nocookie.net/starwars/images/6/62/LukeGreenSaber-MOROTJ.png/revision/latest?cb=20150426200707);">'+
-    //       '</div>';
-
-    var luke = $("<div>")
-    .prepend("<p id='new'>Luke</p>")
-    .add('<img src="./assets/images/luke.jpg" alt="Luke" />')
-    .attr('title', 'Lukas');
-    luke.data({
-        health: 100,
-        name: 'Luke',
-        attack: 20
-    }),
-
-
-$("#characters-to-play").append(luke);
-
-// Create Div for each of the characters
-//     characters will contain attributes with "Health Points", "Attack Power", and "Counter Attack Power"
-//     characters will diplay character name at top, image then Health Points at bottom
-
-// on start all characters will be desplayed in characters to play
-
-// First character selected moves to "your character" row and move remaining characters to "enemies avaliable to attack"
-
-// Second character selected moves to "Defender" row
-
-// Attacking
-    
-//     Your Character Attack Power increases by its base Attack Power on every click.
-//     Defender Health Points is reduced by Your character attack power.
-//     Defender counter attack decreases your character health points at a constant rate
-//     If your character Healthpoints is 0 or less then first character is defeated
-//     If defender Health Points is 0 or less then second character is defeated
-
-
-
-
-// If first character is defeated
-//     attack button is disabled
-//     restart button appears
-
-// If first defender is defeated
-//     remove defender
-//     disable attack button with a message that "no enemy here"
-//     allow user to pick character from "enemies available to attack"
-
-
-
-
-
-
-
-
-
-
+    function startGame() {
+            var randomNumber = Math.floor((Math.random() * 120) + 19);
+            $('#random-number').text(randomNumber);
+            var greenRandomValue = Math.floor((Math.random() * 12) + 1);
+            var purpleRandomValue = Math.floor((Math.random() * 12) + 1);
+            var yellowRandomValue = Math.floor((Math.random() * 12) + 1);
+            var redRandomValue = Math.floor((Math.random() * 12) + 1);
+            // assign random value to each of the crystals at start of game
+            greenCrystal = $('<img>');
+            greenCrystal.attr('data-num', greenRandomValue);
+            greenCrystal.attr('src', './assets/images/Green.jpg');
+            greenCrystal.attr('alt', 'Green Crystal');
+            $('#crystals').append(greenCrystal);
+            purpleCrystal = $('<img>');
+            purpleCrystal.attr('data-num', purpleRandomValue);
+            purpleCrystal.attr('src', './assets/images/Purple.jpg');
+            purpleCrystal.attr('alt', 'Purple Crystal');
+            $('#crystals').append(purpleCrystal);
+            yellowCrystal = $('<img>');
+            yellowCrystal.attr('data-num', yellowRandomValue);
+            yellowCrystal.attr('src', './assets/images/Yellow.jpg');
+            yellowCrystal.attr('alt', 'Yellow Crystal');
+            $('#crystals').append(yellowCrystal);
+            redCrystal = $('<img>');
+            redCrystal.attr('data-num', redRandomValue);
+            redCrystal.attr('src', './assets/images/Red.jpg');
+            redCrystal.attr('alt', 'Red Crystal');
+            $('#crystals').append(redCrystal);
+        }
+        // ----------------------loop option----------------
+        // for (var i = 0; i < randomValue.length; i++) {
+        // var Crystal = $('<img>');
+        // imageCrystal.attr('data-num', numbers[i]);
+        // imageCrystal.attr('src', './assets/images/??????.jpg');
+        // imageCrystal.attr('alt', 'crystals');
+        // $('#crystals').append(Crystal);
+        // }
+        // when one of the crystals is clicked, add value of crystal to user score
+        // if user score is greater than random number then add 1 to losses and restart game
+        // if user score equals random number then add 1 to wins and restart game
+    $('#crystals').on('click', function() {
+        yourScore = yourScore + parseInt($(this).data('num'));
+        $('#your-score').text(yourScore);
+        if (yourScore == randomNumber) {
+            wins++;
+            $('#wins').html("Wins: " + wins);
+            $('#result-announced').html('You won!!!!');
+            reset();
+        } else if (yourScore > randomNumber) {
+            losses++;
+            $('#losses').html("Losses: " + losses);
+            $('#result-announced').html('You lost!');
+            reset();
+        }
+    });
+    // ----------------test scenario--------------
+    // $('#crystals').on('click', function() {
+    //     yourScore++;
+    //     $('#your-score').text(yourScore);
+    //     if (yourScore == 3) {
+    //         wins++;
+    //         $('#wins').html("Wins: " + wins);
+    //         $('#result-announced').html('You won!!!!');
+    //         reset();
+    //     } else if (yourScore > 5) {
+    //         losses++;
+    //         $('#losses').html("Losses: " + losses);
+    //         $('#result-announced').html('You lost!');
+    //         reset();
+    //     }
+    // });
 });
